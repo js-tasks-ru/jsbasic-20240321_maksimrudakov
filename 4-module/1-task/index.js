e720b278a822d3e217c69ee3c3d75fb1f6c844f9
Maksim Rudakov <maksimrudakov@list.ru>
2024-04-08 19:18:31 +0300

    all module-4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,16 @@
 function makeFriendsList(friends) {
-  // ваш код...
+
+   let ul = document.createElement('ul');
+
+  for (let obj of friends) 
+    ul.innerHTML += `<li>${obj.firstName}  ${obj.lastName}</li>`;
+  
+  return ul
+
+//   return friends.reduce( 
+//  (ul, obj) => `<li>${obj.firstName}  ${obj.lastName}</li>`, `` 
+//   )
+
 }
+
+
