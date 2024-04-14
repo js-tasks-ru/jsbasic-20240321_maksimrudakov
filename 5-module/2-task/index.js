7f70a5aec5626e67a591713407418f5691a3a703
Maksim Rudakov <maksimrudakov@list.ru>
2024-04-15 00:20:26 +0300

    added module-5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,13 @@
 function toggleText() {
-  // ваш код...
+  
+  const button = document.querySelector('.toggle-text-button');
+
+  
+  button.addEventListener('click', () => {
+
+    const text = document.querySelector('[id="text"]');
+
+    text.hidden = (text.hidden == false) ? true :
+                                           false
+  })
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,50 @@
 function initCarousel() {
-  // ваш код...
+  
+  const leftButton = document.querySelector('.carousel__arrow_left');
+  const rightButton = document.querySelector('.carousel__arrow_right');
+
+  const carousel = document.querySelector('.carousel__inner');
+  const step = carousel.offsetWidth;
+  let currentStep = 0;
+
+  const countPictures = (carousel.getElementsByClassName('carousel__slide')).length;
+
+
+  if (currentStep == 0) 
+  leftButton.style.display = 'none';
+  
+  
+////////////////////////////////////////////////////////////////////
+
+ rightButton.addEventListener('click', () => {                    
+
+   if (currentStep == 0) 
+   leftButton.style.display = '';
+
+
+    currentStep -= step;
+    carousel.style.transform = `translateX(${currentStep}px)`;
+
+
+    if (currentStep == -step * (countPictures-1)) 
+   rightButton.style.display = 'none';
+  })
+
+////////////////////////////////////////////////////////////////////
+
+  leftButton.addEventListener('click', () => {
+
+    if (currentStep == -step * (countPictures-1)) 
+    rightButton.style.display = '';
+
+
+    currentStep += step;
+    carousel.style.transform = `translateX(${currentStep}px)`;
+
+
+    if (currentStep == 0) 
+    leftButton.style.display = 'none';
+  })
+
+///////////////////////////////////////////////////////////////////
 }
