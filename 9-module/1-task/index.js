33f5c19563deabad522a236161361a6b890ae4f7
Maksim Rudakov <maksimrudakov@list.ru>
2024-05-05 15:43:40 +0300

    finished 9.1

--- 9-module/1-task/index.js
+++ 9-module/1-task/index.js
@@ -1,3 +1,10 @@
 export default function promiseClick(button) {
-  // ваш код...
+  
+     const promise = new Promise((resolve) => {
+      button.addEventListener('click', (event) => { 
+        resolve(event)},
+        {once: true});
+    });
+  
+    return promise;
 }
